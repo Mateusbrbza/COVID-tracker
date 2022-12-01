6b5e37b6ce1a30c0b9c0bdf58f812f942c86a8c9
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-12-01 00:37:08 -0300

    styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
 
 class App extends React.Component {
     render() {
