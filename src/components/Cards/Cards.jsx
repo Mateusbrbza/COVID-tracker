47408ee4fae2678e5d2cdbe1fae4a8db5c7868fe
Mateusbrbza <mateus.c.barboza@gmail.com>
2022-12-09 00:55:06 -0300

    Cards on the webpage

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+
+import styles from './Cards.module.css';
 
 const Cards = (props) => {
   console.log(props);
 
   return (
-    <div>Cards</div>
+    <div className={styles.container} >
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infectados
+            </Typography>
+            <Typography variant="h5" >
+              Dados
+            </Typography>
+            <Typography color="textSecondary">
+              Data
+            </Typography>
+            <Typography variant="body2">
+              Numero de casos ativos de covid
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Mortes
+            </Typography>
+            <Typography variant="h5" >
+              Dados
+            </Typography>
+            <Typography color="textSecondary">
+              Data
+            </Typography>
+            <Typography variant="body2">
+              Numeros de mortes pelo COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
   )
 }
 
